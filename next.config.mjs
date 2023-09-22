419965dffd6ccd0c102ccdc796302afad04e20a9
riso.schwarcz <riso.schwarcz@gmail.com>
2023-09-22 19:01:18 +0200

    feat: allow credentials in headers

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.mjs");
+await import('./src/env.mjs')
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -14,9 +14,19 @@ const config = {
    * @see https://github.com/vercel/next.js/issues/41980
    */
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    locales: ['en'],
+    defaultLocale: 'en',
   },
-};
+  headers: () => {
+    const headers = [
+      {
+        key: 'Access-Control-Allow-Credentials',
+        value: 'true',
+      },
+    ]
+    return headers
+  },
+}
 
-export default config;
+export default config
+
