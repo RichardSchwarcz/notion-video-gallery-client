e824ae659d00269eb1e900f0976ab3c96a51b9e2
riso.schwarcz <riso.schwarcz@gmail.com>
2023-09-25 20:17:31 +0200

    chore: simplify auth page

--- src/pages/authentication.tsx
+++ src/pages/authentication.tsx
@@ -1,7 +1,9 @@
 import { useQuery } from '@tanstack/react-query'
 import Link from 'next/link'
-import { useEffect } from 'react'
-import { UserAuthForm } from '~/components/user-auth-form'
+import { Icons } from '~/components/Icons'
+import { Button, buttonVariants } from '~/components/ui/button'
+import { cn } from '~/lib/utils'
+
 import { URLs } from '~/url'
 
 export default function AuthenticationPage() {
@@ -36,16 +38,6 @@ export default function AuthenticationPage() {
             </svg>
             Acme Inc
           </div>
-          <div className="relative z-20 mt-auto">
-            <blockquote className="space-y-2">
-              <p className="text-lg">
-                &ldquo;This library has saved me countless hours of work and
-                helped me deliver stunning designs to my clients faster than
-                ever before.&rdquo;
-              </p>
-              <footer className="text-sm">Sofia Davis</footer>
-            </blockquote>
-          </div>
         </div>
         <div className="lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
@@ -53,11 +45,17 @@ export default function AuthenticationPage() {
               <h1 className="text-2xl font-semibold tracking-tight">
                 Create an account
               </h1>
-              <p className="text-sm text-muted-foreground">
-                Enter your email below to create your account
-              </p>
             </div>
-            <UserAuthForm />
+
+            {data && (
+              <Link
+                href={data?.googleOAuthURL}
+                className={cn(buttonVariants({ variant: 'default' }))}
+              >
+                Sign in with Google
+              </Link>
+            )}
+
             <p className="px-8 text-center text-sm text-muted-foreground">
               By clicking continue, you agree to our{' '}
               <Link
