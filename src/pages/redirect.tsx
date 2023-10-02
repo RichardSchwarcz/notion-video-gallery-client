a596028051424d740d92e050cb3108451506378a
riso.schwarcz <riso.schwarcz@gmail.com>
2023-10-02 20:01:32 +0200

    style: remove comments

--- src/pages/redirect.tsx
+++ src/pages/redirect.tsx
@@ -1,6 +1,3 @@
-// import type { NextPageContext } from 'next'
-// import { getGoogleOAuthTokens } from '~/getGoogleOAuthTokens'
-// import { serialize } from 'cookie'
 import { Button } from '~/components/ui/button'
 import { URLs } from '~/url'
 
@@ -37,57 +34,3 @@ function Redirect() {
 }
 
 export default Redirect
-
-// export const getServerSideProps = async (context: NextPageContext) => {
-//   // const isProduction = process.env.NODE_ENV === 'production'
-//   // const code = context.query.code as string
-//   // const API_URL = isProduction ? URLs.getTokens.prod : URLs.getTokens.dev
-//   // await fetch(API_URL + code, {
-//   //   credentials: 'include',
-//   // })
-
-//   const code = context.query.code as string
-
-//   try {
-//     const tokens = await getGoogleOAuthTokens(code)
-//     console.log(tokens)
-
-//     if (!tokens) {
-//       throw new Error('Failed to get Google OAuth tokens')
-//     }
-
-//     const accessTokenOptions = {
-//       httpOnly: true,
-//       // maxAge: tokens.expires_in - 200,
-//       path: '/api',
-//       secure: true,
-//     }
-
-//     const refreshTokenOptions = {
-//       httpOnly: true,
-//       maxAge: 60 * 60 * 24 * 30,
-//       path: '/api',
-//       secure: true,
-//     }
-
-//     if (context.res) {
-//       // context.res.setHeader(
-//       //   'Set-Cookie',
-//       //   serialize('access_token', tokens.access_token, accessTokenOptions)
-//       // )
-//       // context.res.setHeader(
-//       //   'Set-Cookie',
-//       //   serialize('refresh_token', tokens.refresh_token, refreshTokenOptions)
-//       // )
-//       context.res.setHeader(
-//         'Set-Cookie',
-//         serialize('access_token', tokens.access_token, accessTokenOptions)
-//       )
-//       // .setHeader('Set-Cookie', serialize('refresh_token', 'kkt'))
-//     }
-//   } catch (error) {
-//     console.error(error)
-//   }
-
-//   return { props: {} }
-// }
